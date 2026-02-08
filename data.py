ac49a5c52d2528ee281e092545989250077a19e2
hannahhfrank <hannah.frank1994@googlemail.com>
2026-02-08 19:03:24 +0100

    u

---
+++ data.py
@@ -0,0 +1,377 @@
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point
+pd.options.mode.chained_assignment = None 
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import patches
+from matplotlib import cm 
+import matplotlib as mpl
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from cycler import cycler
+from sklearn import preprocessing
+import numpy as np
+
+# Plot parameters 
+plot_params = {"text.usetex":True,"font.family":"serif","font.size":15,"xtick.labelsize":15,"ytick.labelsize":15,"axes.labelsize":15,"figure.titlesize":20,"figure.figsize":(8,5),"axes.prop_cycle":cycler(color=['black','rosybrown','gray','indianred','red','maroon','silver',])}
+plt.rcParams.update(plot_params)
+
+### Load ACLED data from local folder --------
+acled = pd.read_csv("data/acled/acled_all_events.csv",low_memory=False,index_col=[0]) 
+
+##########################
+### Get data for India ###
+##########################
+
+### Only include certain events -----
+df_s = acled.loc[(acled['event_type']=="Protests") |
+                (acled['event_type']=="Explosions/Remote violence") |
+                (acled['event_type']=="Riots") |
+                (acled['event_type']=="Strategic developments") |
+                (acled['event_type']=="Battles") |
+                (acled['event_type']=="Violence against civilians") ].copy(deep=True)
+
+### Subset for India -----
+df_protest = df_s.loc[(df_s["country"] == "India") & (df_s["year"]==2016) | 
+                      (df_s["country"] == "India") & (df_s["year"]==2017) | 
+                      (df_s["country"] == "India") & (df_s["year"]==2018) | 
+                      (df_s["country"] == "India") & (df_s["year"]==2019) | 
+                      (df_s["country"] == "India") & (df_s["year"]==2020) |
+                      (df_s["country"] == "India") & (df_s["year"]==2021) |
+                      (df_s["country"] == "India") & (df_s["year"]==2022) |
+                      (df_s["country"] == "India") & (df_s["year"]==2023)].copy(deep=True)
+
+# Check sources for India events. 
+counts = df_protest["source"].value_counts()
+
+### Add dates ----------
+df_protest["dd"] = pd.to_datetime(df_protest['event_date'],format='%d %B %Y')
+df_protest["dd"] = df_protest["dd"].dt.strftime('%Y-%m')
+
+### Add grid ids ---------
+df_protest["gid"] = 0
+df_protest.reset_index(inplace=True, drop=True)
+
+def add_value_if_no_or_one_decimal(x, value_to_add):
+    if x % 1 == 0 or round(x, 1) == x:  # Check if value has no or only one decimal
+        return x + value_to_add
+    else:
+        return x
+
+# Apply the function to the column
+value_to_add = 0.0001  # Define the value to add
+df_protest['longitude'] = df_protest['longitude'].apply(add_value_if_no_or_one_decimal, value_to_add=value_to_add)
+df_protest['latitude'] = df_protest['latitude'].apply(add_value_if_no_or_one_decimal, value_to_add=value_to_add)
+
+### Prio shape file ---------
+prio_shp = gpd.read_file('/Users/hannahfrank/protest_auto_reg/data/acled/priogrid_cell.shp')
+prio_shp.reset_index(inplace=True, drop=True)
+
+### Assign to grid id --------
+for i in range(len(df_protest)):
+    prio_shp_s = prio_shp[(prio_shp["ycoord"]>df_protest.latitude.iloc[i]-1) &
+                              (prio_shp["ycoord"]<df_protest.latitude.iloc[i]+1) &
+                              (prio_shp["xcoord"]>df_protest.longitude.iloc[i]-1) &
+                              (prio_shp["xcoord"]<df_protest.longitude.iloc[i]+1)]
+    prio_shp_s.reset_index(inplace=True, drop=True)
+    prio_shp_s[['gid',"xcoord","ycoord",'geometry']]
+
+    for x in range(len(prio_shp_s)):
+        if prio_shp_s.geometry.iloc[x].contains(Point(df_protest.longitude.iloc[i], df_protest.latitude.iloc[i])) == True:
+            df_protest.gid.iloc[i] = prio_shp_s.gid.iloc[x]
+            break
+            
+###############
+### Protest ###
+###############
+
+### Aggregate to month level -------
+df_s = df_protest.loc[df_protest['event_type']=="Protests"]
+agg_month = pd.DataFrame(df_s.groupby(["dd","year","gid","iso","country"]).size())
+agg_protest = agg_month.reset_index()
+agg_protest.rename(columns={0:"n_protest_events"}, inplace=True)
+print("Aggregated data")
+print(agg_protest.head())
+
+##################################
+### Explosions/remote violence ###
+##################################
+
+### Aggregate to month level -------
+df_s = df_protest.loc[df_protest['event_type']=="Explosions/Remote violence"]
+agg_month = pd.DataFrame(df_s.groupby(["dd","year","gid","iso","country"]).size())
+agg_remote = agg_month.reset_index()
+agg_remote.rename(columns={0:"n_remote_events"}, inplace=True)
+print("Aggregated data")
+print(agg_remote.head())
+
+# Merge
+df=pd.merge(agg_protest, agg_remote[["dd","gid","n_remote_events"]],on=["dd","gid"],how="outer")
+
+#############
+### Riots ###
+#############
+
+### Aggregate to month level -------
+df_s = df_protest.loc[df_protest['event_type']=="Riots"]
+agg_month = pd.DataFrame(df_s.groupby(["dd","year","gid","iso","country"]).size())
+agg_riot = agg_month.reset_index()
+agg_riot.rename(columns={0:"n_riot_events"}, inplace=True)
+print("Aggregated data")
+print(agg_riot.head())
+
+# Merge
+df=pd.merge(df, agg_riot[["dd","gid","n_riot_events"]],on=["dd","gid"],how="outer")
+
+#############################
+### Strategic developmens ###
+#############################
+
+### Aggregate to month level -------
+df_s = df_protest.loc[df_protest['event_type']=="Strategic developments"]
+agg_month = pd.DataFrame(df_s.groupby(["dd","year","gid","iso","country"]).size())
+agg_dev = agg_month.reset_index()
+agg_dev.rename(columns={0:"n_stratdev_events"}, inplace=True)
+print("Aggregated data")
+print(agg_dev.head())
+
+# Merge
+df=pd.merge(df, agg_dev[["dd","gid","n_stratdev_events"]],on=["dd","gid"],how="outer")     
+
+
+###############
+### Battles ###
+###############
+
+### Aggregate to month level -------
+df_s = df_protest.loc[df_protest['event_type']=="Battles"]
+agg_month = pd.DataFrame(df_s.groupby(["dd","year","gid","iso","country"]).size())
+agg_dev = agg_month.reset_index()
+agg_dev.rename(columns={0:"n_battles_events"}, inplace=True)
+print("Aggregated data")
+print(agg_dev.head())
+
+# Merge
+df=pd.merge(df, agg_dev[["dd","gid","n_battles_events"]],on=["dd","gid"],how="outer")     
+
+
+##################################
+### Violence against civilians ###
+##################################
+
+### Aggregate to month level -------
+df_s = df_protest.loc[df_protest['event_type']=="Violence against civilians"]
+agg_month = pd.DataFrame(df_s.groupby(["dd","year","gid","iso","country"]).size())
+agg_dev = agg_month.reset_index()
+agg_dev.rename(columns={0:"n_civilvio_events"}, inplace=True)
+print("Aggregated data")
+print(agg_dev.head())
+
+# Merge
+df=pd.merge(df, agg_dev[["dd","gid","n_civilvio_events"]],on=["dd","gid"],how="outer")     
+
+# Replace missing values
+df.loc[df["dd"]=="2016-01","year"]=2016
+df.loc[df["dd"]=="2016-02","year"]=2016
+df.loc[df["dd"]=="2016-03","year"]=2016
+df.loc[df["dd"]=="2016-04","year"]=2016
+df.loc[df["dd"]=="2016-05","year"]=2016
+df.loc[df["dd"]=="2016-06","year"]=2016
+df.loc[df["dd"]=="2016-07","year"]=2016
+df.loc[df["dd"]=="2016-08","year"]=2016
+df.loc[df["dd"]=="2016-09","year"]=2016
+df.loc[df["dd"]=="2016-10","year"]=2016
+df.loc[df["dd"]=="2016-11","year"]=2016
+df.loc[df["dd"]=="2016-12","year"]=2016
+df.loc[df["dd"]=="2017-01","year"]=2017
+df.loc[df["dd"]=="2017-02","year"]=2017
+df.loc[df["dd"]=="2017-03","year"]=2017
+df.loc[df["dd"]=="2017-04","year"]=2017
+df.loc[df["dd"]=="2017-05","year"]=2017
+df.loc[df["dd"]=="2017-06","year"]=2017
+df.loc[df["dd"]=="2017-07","year"]=2017
+df.loc[df["dd"]=="2017-08","year"]=2017
+df.loc[df["dd"]=="2017-09","year"]=2017
+df.loc[df["dd"]=="2017-10","year"]=2017
+df.loc[df["dd"]=="2017-11","year"]=2017
+df.loc[df["dd"]=="2017-12","year"]=2017
+df.loc[df["dd"]=="2018-01","year"]=2018
+df.loc[df["dd"]=="2018-02","year"]=2018
+df.loc[df["dd"]=="2018-03","year"]=2018
+df.loc[df["dd"]=="2018-04","year"]=2018
+df.loc[df["dd"]=="2018-05","year"]=2018
+df.loc[df["dd"]=="2018-06","year"]=2018
+df.loc[df["dd"]=="2018-07","year"]=2018
+df.loc[df["dd"]=="2018-08","year"]=2018
+df.loc[df["dd"]=="2018-09","year"]=2018
+df.loc[df["dd"]=="2018-10","year"]=2018
+df.loc[df["dd"]=="2018-11","year"]=2018
+df.loc[df["dd"]=="2018-12","year"]=2018
+df.loc[df["dd"]=="2019-01","year"]=2019
+df.loc[df["dd"]=="2019-02","year"]=2019
+df.loc[df["dd"]=="2019-03","year"]=2019
+df.loc[df["dd"]=="2019-04","year"]=2019
+df.loc[df["dd"]=="2019-05","year"]=2019
+df.loc[df["dd"]=="2019-06","year"]=2019
+df.loc[df["dd"]=="2019-07","year"]=2019
+df.loc[df["dd"]=="2019-08","year"]=2019
+df.loc[df["dd"]=="2019-09","year"]=2019
+df.loc[df["dd"]=="2019-10","year"]=2019
+df.loc[df["dd"]=="2019-11","year"]=2019
+df.loc[df["dd"]=="2019-12","year"]=2019
+df.loc[df["dd"]=="2020-01","year"]=2020
+df.loc[df["dd"]=="2020-02","year"]=2020
+df.loc[df["dd"]=="2020-03","year"]=2020
+df.loc[df["dd"]=="2020-04","year"]=2020
+df.loc[df["dd"]=="2020-05","year"]=2020
+df.loc[df["dd"]=="2020-06","year"]=2020
+df.loc[df["dd"]=="2020-07","year"]=2020
+df.loc[df["dd"]=="2020-08","year"]=2020
+df.loc[df["dd"]=="2020-09","year"]=2020
+df.loc[df["dd"]=="2020-10","year"]=2020
+df.loc[df["dd"]=="2020-11","year"]=2020
+df.loc[df["dd"]=="2020-12","year"]=2020
+df.loc[df["dd"]=="2021-01","year"]=2021
+df.loc[df["dd"]=="2021-02","year"]=2021
+df.loc[df["dd"]=="2021-03","year"]=2021
+df.loc[df["dd"]=="2021-04","year"]=2021
+df.loc[df["dd"]=="2021-05","year"]=2021
+df.loc[df["dd"]=="2021-06","year"]=2021
+df.loc[df["dd"]=="2021-07","year"]=2021
+df.loc[df["dd"]=="2021-08","year"]=2021
+df.loc[df["dd"]=="2021-09","year"]=2021
+df.loc[df["dd"]=="2021-10","year"]=2021
+df.loc[df["dd"]=="2021-11","year"]=2021
+df.loc[df["dd"]=="2021-12","year"]=2021
+df.loc[df["dd"]=="2022-01","year"]=2022
+df.loc[df["dd"]=="2022-02","year"]=2022
+df.loc[df["dd"]=="2022-03","year"]=2022
+df.loc[df["dd"]=="2022-04","year"]=2022
+df.loc[df["dd"]=="2022-05","year"]=2022
+df.loc[df["dd"]=="2022-06","year"]=2022
+df.loc[df["dd"]=="2022-07","year"]=2022
+df.loc[df["dd"]=="2022-08","year"]=2022
+df.loc[df["dd"]=="2022-09","year"]=2022
+df.loc[df["dd"]=="2022-10","year"]=2022
+df.loc[df["dd"]=="2022-11","year"]=2022
+df.loc[df["dd"]=="2022-12","year"]=2022
+df.loc[df["dd"]=="2023-01","year"]=2023
+df.loc[df["dd"]=="2023-02","year"]=2023
+df.loc[df["dd"]=="2023-03","year"]=2023
+df.loc[df["dd"]=="2023-04","year"]=2023
+df.loc[df["dd"]=="2023-05","year"]=2023
+df.loc[df["dd"]=="2023-06","year"]=2023
+df.loc[df["dd"]=="2023-07","year"]=2023
+df.loc[df["dd"]=="2023-08","year"]=2023
+df.loc[df["dd"]=="2023-09","year"]=2023
+df.loc[df["dd"]=="2023-10","year"]=2023
+df.loc[df["dd"]=="2023-11","year"]=2023
+df.loc[df["dd"]=="2023-12","year"]=2023
+df["iso"]=356
+df["country"]="India"
+df=df.fillna(0)
+
+### Get countries and years --------
+prio_help = gpd.read_file("/Users/hannahfrank/protest_auto_reg/data/acled/prio_time_2014.csv")
+prio_help_s=prio_help[["gid"]].loc[(prio_help["gwno"]=="750")].reset_index(drop=True)
+prio_help_s["gid"]=prio_help_s["gid"].astype(int)
+gid = list(prio_help_s.gid.unique())
+
+### Make range of time stamps to add missing observations ------
+date = ['2016-01','2016-02','2016-03','2016-04','2016-05','2016-06','2016-07','2016-08','2016-09','2016-10','2016-11','2016-12','2017-01','2017-02','2017-03','2017-04','2017-05','2017-06','2017-07','2017-08','2017-09','2017-10','2017-11','2017-12','2018-01','2018-02','2018-03','2018-04','2018-05','2018-06','2018-07','2018-08','2018-09','2018-10','2018-11','2018-12','2019-01','2019-02','2019-03','2019-04','2019-05','2019-06','2019-07','2019-08','2019-09','2019-10','2019-11','2019-12','2020-01','2020-02','2020-03','2020-04','2020-05','2020-06','2020-07','2020-08','2020-09','2020-10','2020-11','2020-12','2021-01','2021-02','2021-03','2021-04','2021-05','2021-06','2021-07','2021-08','2021-09','2021-10','2021-11','2021-12','2022-01','2022-02','2022-03','2022-04','2022-05','2022-06','2022-07','2022-08','2022-09','2022-10','2022-11','2022-12','2023-01','2023-02','2023-03','2023-04','2023-05','2023-06','2023-07','2023-08','2023-09','2023-10','2023-11','2023-12']
+
+### Add missing observation to time series, those have zero fatalities --------
+
+# Loop through every country-month
+for i in range(0, len(gid)):
+    #print(gid[i])
+    for x in range(0, len(date)):
+        #print(date[x])
+        
+        # Check if country-month in data, if False add
+        if ((df['dd']==date[x]) 
+            & (df['gid']==gid[i])).any()==False:
+                
+                # Subset data to add
+                s = {'dd':date[x],'year':int(date[x][0:4]),'gid':gid[i],'country':"India",'iso':356,'n_protest_events':0,"n_remote_events":0,"n_riot_events":0,"n_stratdev_events":0,"n_battles_events":0,"n_civilvio_events":0}
+                s = pd.DataFrame(data=s,index=[0])
+                df = pd.concat([df,s])  
+print("Added missing obersvationes")
+
+# Sort 
+df = df[df["gid"].isin(gid)]
+df = df.sort_values(by=["gid","dd"])
+df=df.reset_index(drop=True)
+df.isnull().any()
+
+#####################
+### Add PRIO data ###
+#####################
+
+prio_static = gpd.read_file('/Users/hannahfrank/protest_auto_reg/data/acled/prio_static.csv')
+prio_static = prio_static.drop(columns=['row','col',"xcoord","ycoord","geometry","cmr_max","cmr_min","cmr_sd","rainseas","maincrop","growend","growstart","imr_max","imr_min","imr_sd","ttime_max","ttime_min","ttime_sd",])
+prio_static.replace([''], np.nan, inplace=True)
+
+### Variables in prio grid ###
+# landarea: total area covered by land in the grid cell in square kilometers 
+# ttime_: travel time to the nearest major city
+# mountain_mean:  proportion of mountainous terrain within the cell
+# imr_: measures infant mortality rate
+# cmr_: prevalence of child malnutrition
+# petroleum_s: dummy variable for whether onshore petroleum deposits have been found within the given grid cell
+# diamsec_s: dummy variable for whether secondary (alluvial) diamond deposits have been found within the given grid cell
+# diamprim_s: dummy variable for whether primary (kimberlite) diamond deposits have been found within the given grid cell
+# goldplacer_s: dummy variable for whether placer gold deposits have been found within the given grid cell
+# goldsurface_s: dummy variable for whether surface gold deposits have been found within the given grid cell
+# goldvein_s: dummy variable for whether vein gold deposits have been found within the given grid cell
+# gem_s: dummy variable for whether gem deposits have been found within the given grid cell
+# urban_gc: coverage of urban areas in each cell
+# agri_gc: coverage of agricultural areas in each cell
+# forest_gc: coverage of forest areas in each cell
+# shrub_gc: coverage of shrubland in each cell
+# herb_gc: coverage of herbaceous vegetation and lichens/mosses in each cel
+# aquaveg_gc: coverage of aquatic vegetation in each cell
+# barren_gc: coverage of barren areas in each cell
+# water_gc: coverage of water areas in each cell
+# maincrop: indicates the main crop code for the cell
+# harvarea: sum of the harvested area (given in hectares) for the cell’s main crop 
+# rainseas: initial month of the rainy season in the cell
+# growstart: starting month of the growing season for the cell’s main crop
+# growend: final month of the growing season for the cell’s main crop
+
+### Merge -----
+prio_static['gid']=prio_static['gid'].astype(int)
+df['gid']=df['gid'].astype(int)
+df=pd.merge(df,prio_static, how='left', on='gid')
+df = df.dropna(axis=1)
+
+### Save -------
+df.isnull().any()
+df.to_csv("/Users/hannahfrank/protest_auto_reg/data/acled/acled_grid_India_2023.csv",index=False, sep=',')
+
+#############
+### Plots ###
+#############
+
+prio_shp = gpd.read_file('/Users/hannahfrank/protest_auto_reg/data/acled/priogrid_cell.shp')
+prio_shp.reset_index(inplace=True, drop=True)
+india = df.merge(prio_shp[["gid", "geometry"]],left_on='gid',right_on='gid',how="left")
+india = gpd.GeoDataFrame(india, geometry="geometry")
+
+#######################
+### Actuals protest ###
+#######################
+
+for i in ['2016-01','2016-02','2016-03','2016-04','2016-05','2016-06','2016-07','2016-08','2016-09','2016-10','2016-11','2016-12','2017-01','2017-02','2017-03','2017-04','2017-05','2017-06','2017-07','2017-08','2017-09','2017-10','2017-11','2017-12','2018-01','2018-02','2018-03','2018-04','2018-05','2018-06','2018-07','2018-08','2018-09','2018-10','2018-11','2018-12','2019-01','2019-02','2019-03','2019-04','2019-05','2019-06','2019-07','2019-08','2019-09','2019-10','2019-11','2019-12','2020-01','2020-02','2020-03','2020-04','2020-05','2020-06','2020-07','2020-08','2020-09','2020-10','2020-11','2020-12','2021-01','2021-02','2021-03','2021-04','2021-05','2021-06','2021-07','2021-08','2021-09','2021-10','2021-11','2021-12','2022-01','2022-02','2022-03','2022-04','2022-05','2022-06','2022-07','2022-08','2022-09','2022-10','2022-11','2022-12','2023-01','2023-02','2023-03','2023-04','2023-05','2023-06','2023-07','2023-08','2023-09','2023-10','2023-11','2023-12']:
+    fig, ax = plt.subplots(figsize = (10, 10))
+    plt.axis('off')
+    india.loc[(india["dd"]==i)].plot(column='n_protest_events',ax=ax,cmap="Blues",edgecolor='gray',linewidth=0.3)
+    cmap = plt.cm.get_cmap('Blues')
+    plt.title(f"Number of protest events in {i}")
+    norm = mpl.colors.Normalize(vmin=0, vmax=india.loc[(india["dd"]==i)].n_protest_events.max())
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="2%", pad=0.01)   
+    colorbar=plt.colorbar(cm.ScalarMappable(cmap=cmap,norm=norm),ax=ax,cax=cax)
+    colorbar.ax.tick_params(axis='y', labelsize=20)  
+    plt.show()
+      
